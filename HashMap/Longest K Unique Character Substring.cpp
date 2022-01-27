a8009d644d7450044282f956b83e2340b481bf74
Shubhi3199 <44928935+Shubhi3199@users.noreply.github.com>
2022-01-27 16:45:45 +0530

    Create Longest K Unique Character Substring.cpp

---
+++ HashMap/Longest K Unique Character Substring.cpp
@@ -0,0 +1,20 @@
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int start = 0, end = 0;
+        unordered_map<char, int> freq_map;
+        int max_size = -1;
+        while (end < s.size()) {
+            freq_map[s[end]]++;
+            if (freq_map.size() == k) max_size = max(max_size, end - start  + 1);
+            while (freq_map.size() > k) {
+                freq_map[s[start]]--;
+                if (freq_map[s[start]] == 0) freq_map.erase(s[start]);
+                start++;
+            }
+            end++;
+        }
+        if (max_size != -1) max_size = max(max_size, end - start);
+        return max_size;
+    }
+};
