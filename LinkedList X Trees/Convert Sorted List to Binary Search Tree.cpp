7b91cdeadab5c9723155c241c80b646b7d647b01
Shubhi3199 <44928935+Shubhi3199@users.noreply.github.com>
2022-02-01 08:00:54 +0530

    Create Convert Sorted List to Binary Search Tree.cpp

---
+++ LinkedList X Trees/Convert Sorted List to Binary Search Tree.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/submissions/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<ListNode*, ListNode*> get_mid(ListNode* head) {
+        ListNode* slow = head, *fast = head, *prev = NULL;
+        while (fast != NULL and fast -> next != NULL) {
+            prev = slow;
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return {prev, slow};
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (head == NULL) return NULL;
+        // Step1: Get the mif node fron the current head and that mid will become the root of the current subtree as it will divide the LL in 2 equal left and right halfs and ifc we do this step recursively then everytime we'll have almost equal halves ensuring a height balanced BST
+        pair<ListNode*, ListNode*> mid_pair = get_mid(head);
+        ListNode* prev = mid_pair.first;
+        ListNode* mid = mid_pair.second;
+        TreeNode* root = new TreeNode(mid -> val);
+        // Now mark prev -> next to NULL before passing head to the left half recursively
+        if (prev) {
+            prev -> next = NULL;
+            root -> left = sortedListToBST(head); // we make left call only wjen prev != NULL as otherwise there must be just 1 node in the left LL which is alreadt the basce case
+        }
+        root -> right = sortedListToBST(mid -> next);
+        return root;
+    }
+};
